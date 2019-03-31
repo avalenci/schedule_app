16722d863f16ca3b4741877878a13bdd9e9cb2b6
Valenci, Andrew <ansmeew@gmail.com>
2019-03-31 11:20:38 -0400

    added arrayList

--- app/src/main/java/com/example/schedule_app/Table.java
+++ app/src/main/java/com/example/schedule_app/Table.java
@@ -3,13 +3,28 @@ package com.example.schedule_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Table extends AppCompatActivity {
 
+    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
+
+        listview = (ListView) findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        //Add entries here
+        arrayList.add("");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listview.setAdapter(arrayAdapter);
     }
 
 }
