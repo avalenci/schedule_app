87354853bc9551fdd19e9bdb82e12481f472810f
Tom B <thomasbarrett603@hotmail.com>
2019-03-30 20:32:19 -0400

    Course commit

---
+++ app/src/main/java/com/example/schedule_app/Course.java
@@ -0,0 +1,20 @@
+package com.example.schedule_app;
+
+import java.sql.Time;
+import java.util.Timer;
+
+public class Course {
+
+    private String name;
+
+
+    private String setName(String _name) {
+        this.name = _name;
+        return _name;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+}
